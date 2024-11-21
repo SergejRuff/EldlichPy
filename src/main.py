88a73bd1e722b719acc4906194f9c6bba773f091
SergejRuff <serijnh@gmail.com>
2024-11-21 23:23:30 +0100

    now stories can be read in terminal

--- src/main.py
+++ src/main.py
@@ -8,16 +8,16 @@ Created on Thu Nov 21 20:58:50 2024
 
 from utils import fetch_lovecraft
 from sys import argv
-import re 
 
 def main():
-    #url = argv[1]
-    url = "https://www.hplovecraft.com/writings/texts/fiction/b.aspx"
+    url = argv[1]
+    #url = "https://www.hplovecraft.com/writings/texts/fiction/b.aspx"
     print(f"the current url: \n{url}\n")
     hp_text = fetch_lovecraft(url)
-    #print(hp_text)
-    title = re.search(r'<title>"(.*?)"',hp_text).group(1)
-    print(f"the title of the selected work: \n'{title}' by H.P. Lovecraft")
+    title_text = hp_text["title"]
+    print(f"the title of the selected work: \n'{title_text}' by H.P. Lovecraft.")
+    print(hp_text["text"])
+     
     
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,22 @@ Created on Thu Nov 21 21:25:13 2024
 """
 
 import requests
+import re 
 
 def fetch_lovecraft(url):
     hp_text = requests.get(url)
-    return(hp_text.text)
+    title_story = re.search(r'<title>"(.*?)"',hp_text.text).group(1)
+    
+    match = re.search(r'<div align="justify">(.*?)</div>', 
+                      hp_text.text, re.DOTALL)
+    
+    text_content = match.group(1)
+    text_content = re.sub(r'<.*?>', '', text_content)
+    
+    text_content = text_content.replace("&mdash;", "—").replace("&lt;", "<").replace("&gt;", ">")\
+                           .replace("&amp;", "&").replace("&rdquo;", "”").replace("&ldquo;", "“")\
+                           .replace("&rsquo;", "’").replace("&lsquo;", "‘").replace("&nbsp;", " ").replace("&quot;", '"')\
+                           .replace("&apos;", "'").replace("&uuml;", "ü").replace("&eacute;", "é")
+
+
+    return({"title":title_story,"text":text_content})
